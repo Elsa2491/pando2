ff6ce0343d69de63526c42a56311265c41cc7244
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2022-10-06 16:36:10 +0200

    multiple graphs on one canva. CO2 measures for all rooms

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -1,25 +1,69 @@
 class MeasuresController < ApplicationController
   def extract_data
     @measure_room_8a = CSV.parse(File.read("./public/room_8a.csv"), headers: true)
-    @measure_room_8f = CSV.read("./public/room_8f.csv", headers: true)
-    i = 0
-    @data_keys_co2 = []
-    @data_co2 = []
-    @data_hum = []
-    @data_tmp = []
-    @data_voct = []
+    @measure_room_8f = CSV.parse(File.read("./public/room_8f.csv"), headers: true)
+    @measure_room_b2 = CSV.parse(File.read("./public/room_b2.csv"), headers: true)
 
-    @data = @measure_room_8a['measure_type'].each do |tab|
+    i = 0
+    @data_keys = []
+    @data_co2_room_8a  = []
+    @data_hum_room_8a  = []
+    @data_tmp_room_8a  = []
+    @data_voct_room_8a  = []
+    @data_room_8a_room_8a  = @measure_room_8a['measure_type'].each do |tab|
+      @data_keys  << @measure_room_8a['@timestamp'][i].to_datetime.to_i
       case tab
       when 'CO2'
-        @data_keys_co2 << @measure_room_8a['@timestamp'][i].to_datetime.to_i
-        @data_co2 << @measure_room_8a['measure_float'][i]
-      when 'HUM'
-        @data_hum << @measure_room_8a['measure_float'][i]
-      when 'TMP'
-        @data_tmp << @measure_room_8a['measure_float'][i]
-      when 'VOCT'
-        @data_voct << @measure_room_8a['measure_float'][i]
+        @data_co2_room_8a << @measure_room_8a['measure_float'][i]
+      # when 'HUM'
+      #   @data_hum_room_8a  << @measure_room_8a['measure_float'][i]
+      # when 'TMP'
+      #   @data_tmp_room_8a  << @measure_room_8a['measure_float'][i]
+      # when 'VOCT'
+      #   @data_voct_room_8a  << @measure_room_8a['measure_float'][i]
+      end
+      i += 1
+    end
+
+    x = 0
+    @data_keys = []
+    @data_co2_room_8f = []
+    @data_hum_room_8f = []
+    @data_tmp_room_8f = []
+    @data_voct_room_8f = []
+    @data_room_8f = @measure_room_8f['measure_type'].each do |tab|
+      @data_keys << @measure_room_8f['@timestamp'][x].to_datetime.to_i
+      case tab
+      when 'CO2'
+        @data_co2_room_8f << @measure_room_8f['measure_float'][x]
+      # when 'HUM'
+      #   @data_hum_room_8f << @measure_room_8f['measure_float'][x]
+      # when 'TMP'
+      #   @data_tmp_room_8f << @measure_room_8f['measure_float'][x]
+      # when 'VOCT'
+      #   @data_voct_room_8f << @measure_room_8f['measure_float'][x]
+      end
+      i += 1
+    end
+
+    y = 0
+    @data_keys = []
+    @data_co2_room_b2 = []
+    @data_hum_room_b2 = []
+    @data_tmp_room_b2 = []
+    @data_voct_room_b2 = []
+    @data_room_b2 = @measure_room_b2['measure_type'].each do |tab|
+      @data_keys << @measure_room_b2['@timestamp'][y].to_datetime.to_i
+      # binding.pry
+      case tab
+      when 'CO2'
+        @data_co2_room_b2 << @measure_room_b2['measure_float'][y]
+      # when 'HUM'
+      #   @data_hum_room_b2 << @measure_room_b2['measure_float'][y]
+      # when 'TMP'
+      #   @data_tmp_room_b2 << @measure_room_b2['measure_float'][y]
+      # when 'VOCT'
+      #   @data_voct_room_b2 << @measure_room_b2['measure_float'][y]
       end
       i += 1
     end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,27 +20,96 @@ import "controllers"
 import "bootstrap"
 import Chart from 'chart.js/auto';
 
-
+// CO2 measures
 document.addEventListener('turbolinks:load', () => {
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var myChart = new Chart(ctx, {
+  var ctxRoom8A = document.getElementById('ctxRoom8ACo2').getContext('2d');
+  var ctx2 = document.getElementById('myChart2').getContext('2d');
+  var ctx3 = document.getElementById('myChart3').getContext('2d');
+  var myChart = new Chart(ctxRoom8A, {
   data: {
-    labels: JSON.parse(ctx.canvas.dataset.labels),
+    labels: JSON.parse(ctxRoom8A.canvas.dataset.labels),
     datasets: [{
       type: 'line',
-      label: 'salle 1',
-      data: JSON.parse(ctx.canvas.dataset.data),
+      label: 'room_8a',
+      data: JSON.parse(ctxRoom8A.canvas.dataset.data),
       backgroundColor: [
           'rgba(54, 162, 235, 0.2)',
       ],
       borderColor: [
           'rgba(54, 162, 235, 1)',
       ],
+      borderWidth: 1,
+      yAxisID: 'y-axis-2'
+    },
+    {
+      type: 'line',
+      label: 'room_8f',
+      data: JSON.parse(ctx2.canvas.dataset.data),
+      backgroundColor: [
+          'rgba(255, 206, 86, 0.2)',
+      ],
+      borderColor: [
+          'rgba(255, 206, 86, 1)',
+      ],
       borderWidth: 1
     },
-    ]
+     {
+      type: 'line',
+      label: 'room_b2',
+      data: JSON.parse(ctx3.canvas.dataset.data),
+      backgroundColor: [
+          'rgba(75, 192, 192, 0.2)',
+      ],
+      borderColor: [
+          'rgba(75, 192, 192, 1)',
+      ],
+      borderWidth: 1
+    },
+    // {
+    //   type: 'line',
+    //   label: 'voct',
+    //   data: JSON.parse(ctx4.canvas.dataset.data),
+    //   backgroundColor: [
+    //       'rgba(153, 102, 255, 0.2)',
+    //   ],
+    //   borderColor: [
+    //       'rgba(153, 102, 255, 1)',
+    //   ],
+    //   borderWidth: 1
+    // },
+    ],
   },
   });
+  // var myChart2 = new Chart(ctx2, {
+  // data: {
+  //   labels: JSON.parse(ctx2.canvas.dataset.labels),
+  //   datasets: [{
+  //     type: 'line',
+  //     label: 'co2',
+  //     data: JSON.parse(ctx2.canvas.dataset.data),
+  //     backgroundColor: [
+  //         'rgba(54, 162, 235, 0.2)',
+  //     ],
+  //     borderColor: [
+  //         'rgba(54, 162, 235, 1)',
+  //     ],
+  //     borderWidth: 1
+  //   },
+  //   {
+  //     type: 'line',
+  //     label: 'hum',
+  //     data: JSON.parse(ctx2.canvas.dataset.data),
+  //     backgroundColor: [
+  //         'rgba(255, 206, 86, 0.2)',
+  //     ],
+  //     borderColor: [
+  //         'rgba(255, 206, 86, 1)',
+  //     ],
+  //     borderWidth: 1
+  //   },
+  //   ],
+  // },
+  // });
   options: {
     scales: {
         y: {
@@ -48,5 +117,94 @@ document.addEventListener('turbolinks:load', () => {
         }
     }
   }
-})
+}
+)
+
+// document.addEventListener('turbolinks:load', () => {
+//   var ctx = document.getElementById('myChart2').getContext('2d');
+//   console.log(JSON.parse(ctx.canvas.dataset.data))
+//   var myChart2 = new Chart(ctx, {
+//   data: {
+//     labels: JSON.parse(ctx.canvas.dataset.labels),
+//     datasets: [{
+//       type: 'line',
+//       label: 'hum',
+//       data: JSON.parse(ctx.canvas.dataset.data),
+//       backgroundColor: [
+//           'rgba(255, 206, 86, 0.2)',
+//       ],
+//       borderColor: [
+//           'rgba(255, 206, 86, 1)',
+//       ],
+//       borderWidth: 1
+//     },
+//     ],
+//   },
+//   });
+//   options: {
+//     scales: {
+//         y: {
+//             beginAtZero: true
+//         }
+//     }
+//   }
+// })
+
+// document.addEventListener('turbolinks:load', () => {
+//   var ctx = document.getElementById('myChart3').getContext('2d');
+//   var myChart3 = new Chart(ctx, {
+//   data: {
+//     labels: JSON.parse(ctx.canvas.dataset.labels),
+//     datasets: [{
+//       type: 'line',
+//       label: 'tmp',
+//       data: JSON.parse(ctx.canvas.dataset.data),
+//       backgroundColor: [
+//           'rgba(75, 192, 192, 0.2)',
+//       ],
+//       borderColor: [
+//           'rgba(75, 192, 192, 1)',
+//       ],
+//       borderWidth: 1
+//     },
+//     ],
+//   },
+//   });
+//   options: {
+//     scales: {
+//         y: {
+//             beginAtZero: true
+//         }
+//     }
+//   }
+// })
+
+// document.addEventListener('turbolinks:load', () => {
+//   var ctx = document.getElementById('myChart4').getContext('2d');
+//   var myChart4 = new Chart(ctx, {
+//   data: {
+//     labels: JSON.parse(ctx.canvas.dataset.labels),
+//     datasets: [{
+//       type: 'line',
+//       label: 'voct',
+//       data: JSON.parse(ctx.canvas.dataset.data),
+//       backgroundColor: [
+//           'rgba(153, 102, 255, 0.2)',
+//       ],
+//       borderColor: [
+//           'rgba(153, 102, 255, 1)',
+//       ],
+//       borderWidth: 1
+//     },
+//     ],
+//   },
+//   });
+//   options: {
+//     scales: {
+//         y: {
+//             beginAtZero: true
+//         }
+//     }
+//   }
+// })
 
